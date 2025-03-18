70cc41069c250b294249cd5ecbd35d378ecd5a9f
laura magallanes <pasocenturiontours@gmail.com>
2025-03-18 16:59:21 -0300

    Nuevos cambios en ReservaRepository

--- src/main/java/com/grupo1/pidh/repository/ReservaRepository.java
+++ src/main/java/com/grupo1/pidh/repository/ReservaRepository.java
@@ -3,12 +3,13 @@ package com.grupo1.pidh.repository;
 import com.grupo1.pidh.entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ReservaRepository  extends JpaRepository<Reserva, Long> {
-    @Query("SELECT r FROM Reserva r WHERE r.usuario.email = :usuarioEmail ORDER BY r.id DESC")
-    Reserva findTopByUsuarioEmailOrderByIdDesc(String usuarioEmail);
+    @Query(value = "SELECT r FROM Reserva r WHERE r.usuario.email = :usuarioEmail ORDER BY r.id DESC LIMIT 1", nativeQuery = true)
+    Reserva findTopByUsuarioEmailOrderByIdDesc(@Param("usuarioEmail") String usuarioEmail);
 }
