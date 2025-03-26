c1cd6e50a21e00e371d100def33ea0f321754aef
Matheo <matheosantana30@gmail.com>
2025-03-26 14:28:10 -0300

    ms crud reservas completo

--- src/main/java/com/grupo1/pidh/repository/ReservaRepository.java
+++ src/main/java/com/grupo1/pidh/repository/ReservaRepository.java
@@ -1,6 +1,9 @@
 package com.grupo1.pidh.repository;
 
+import com.grupo1.pidh.entity.DisponibilidadProducto;
+import com.grupo1.pidh.entity.Producto;
 import com.grupo1.pidh.entity.Reserva;
+import com.grupo1.pidh.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,6 +20,12 @@ public interface ReservaRepository  extends JpaRepository<Reserva, Long> {
     @Query("SELECT r FROM Reserva r WHERE r.disponibilidadProducto.producto.id = :productoId AND r.puntuacion IS NOT NULL")
     List<Reserva> findResenasByProductoId(@Param("productoId") Long productoId);
 
+
+    List<Reserva> findByUsuario(Usuario usuario);
+    List<Reserva> findByDisponibilidadProducto_Producto(Producto disponibilidadProducto);
+    List<Reserva> findByDisponibilidadProducto(DisponibilidadProducto disponibilidadProducto);
+
     //Optional<Reserva> findByUsuarioEmailAndDisponibilidadProducto_Producto_Id(String usuarioEmail, Long disponibilidadProductoId);
     Optional<Reserva> findByIdAndUsuarioEmail(Long id, String email);
+
 }
